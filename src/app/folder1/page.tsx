c0d46db3ca620290c916b17008f99cec40867815
rumanstheddy <sumanthcob@gmail.com>
2023-08-29 19:06:10 -0400

    fetching data from random user API in client component

--- src/app/folder1/page.tsx
+++ src/app/folder1/page.tsx
@@ -1,11 +1,22 @@
+"use client";
 import Link from "next/link";
-import { Fragment } from "react";
 
 export default function Page() {
+  const getUser = async () => {
+    const response = await fetch("https://randomuser.me/api/");
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
-    <Fragment>
+    <div className="content-center">
       <h1 className="text-2xl mb-10">{"You are in Folder 1."}</h1>
-      <Link href={"/"} className="italic">Back</Link>
-    </Fragment>
+      <Link href={"/"} className="italic">
+        Back
+      </Link>
+      <button type="button" onClick={getUser} className="block rounded-full">
+        Generate User
+      </button>
+    </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
-import { Fragment } from "react";
 
 export default function Home() {
   return (
-    <Fragment>
+    <div className="content-center">
       <h1 className="text-2xl mb-10">{"Hello Sumanth, what's next?"}</h1>
-      <Link href={"/folder1"} className="italic">Go to folder 1</Link>
-    </Fragment>
+      <Link href={"/folder1"} className="italic">
+        Go to folder 1
+      </Link>
+    </div>
   );
 }
