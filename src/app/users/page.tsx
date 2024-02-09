4702e5d8a57a542411da7fefe7fae7058ead0b57
rumanstheddy <sumanthcob@gmail.com>
2024-02-08 20:10:59 -0500

    used an external REST API to get dummy users

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,10 +1,26 @@
 import ProductCard from "../components/ProductCard";
 
-const UsersPage = () => {
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await response.json();
+
   return (
     <div>
       <p>Users Page</p>
       <ProductCard />
+      <br />
+      <ul>
+        {users.map(({ id, name }) => (
+          <li key={id}>
+            {id}: {name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
