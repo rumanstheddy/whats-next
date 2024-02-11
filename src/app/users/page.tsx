08ea2cc26de0213a890256c015de02c32eb4ea0e
rumanstheddy <sumanthcob@gmail.com>
2024-02-10 21:32:24 -0500

    fetch caching

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -6,7 +6,9 @@ interface User {
 }
 
 const UsersPage = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const response = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "default",
+  });
   const users: User[] = await response.json();
 
   return (
